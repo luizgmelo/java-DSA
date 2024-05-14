0c9fb9c92b45778048388a4912fd6813d5294858
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-05-14 11:50:15 -0300

    feat: add remove operations per (last, first, index, object)

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -1,5 +1,7 @@
 package linkedlist;
 
+import java.util.NoSuchElementException;
+
 public class LinkedList {
 	private Node head;
 	private Node tail;
@@ -61,6 +63,80 @@ public class LinkedList {
 			aux.setNext(newNode);
 			this.size++;
 		}
+	}
+	
+	public Person removeFirst() {
+		if (isEmpty()) throw new NoSuchElementException();
+		
+		Person removed = this.head.getData();
+		this.size--;
+	
+		if (this.head == this.tail) {
+			this.head = null;
+			this.tail = null;
+			return removed;
+		}
+		
+		this.head = this.head.getNext();
+		this.head.setPrev(null);
+		return removed;
+	}
+	
+	public Person removeLast() {
+		if (isEmpty()) throw new NoSuchElementException();
+		
+		Person removed = this.tail.getData();
+		this.size--;
+		
+		if (this.head == this.tail) {
+			this.head = null;
+			this.tail = null;
+			return removed;
+		}
+		
+		this.tail = this.tail.getPrev();
+		this.tail.setNext(null);
+		return removed;
+	}
+	
+	public Person remove(int index) {
+		if (index < 0 || index >= this.size) throw new IndexOutOfBoundsException();
+		
+		if (index == 0) return removeFirst();
+		if (index == this.size - 1) return removeLast();
+		
+		Node aux = this.head;
+		// remove (take previous Removed)
+		for (int i = 0; i < index - 1; i++) {
+			aux = aux.getNext();
+		}
+		Person removed = aux.getNext().getData();
+		aux.setNext(aux.getNext().getNext());
+		// aux.next.prev = aux
+		aux.getNext().setPrev(aux.getNext().getPrev());
+		this.size--;
+		return removed;
+	}
+	
+	public boolean remove(Person person) {
+		if (isEmpty()) return false;
+		
+		Node aux = this.head;
+		for (int i = 0; i < this.size; i++) {
+			if (aux.getData().equals(person)) {
+				if (i == 0) removeFirst();
+				else if (i == this.size - 1) removeLast();
+				else {
+					// remove per index different way (take removed)
+					aux.getPrev().setNext(aux.getNext());
+					aux.getNext().setPrev(aux.getPrev());
+					this.size--;
+				}
+				return true;
+			}
+			aux = aux.getNext();
+		}
+		return false;
 	}
 	
 	public Person get(int index) {
