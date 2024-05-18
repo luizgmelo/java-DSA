d24db0ce4459b403f542c68493fc88da76b06d4b
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-05-18 09:09:39 -0300

    feat: add height bst

--- src/BST/BinarySearchTree.java
+++ src/BST/BinarySearchTree.java
@@ -112,5 +112,14 @@ public class BinarySearchTree {
 		}
 	}
 	
+	public int height() {
+		return this.height(this.root);
+	}
+	
+	private int height(Node node) {
+		if (node == null) return -1;
+		return 1 + Math.max(height(node.left), height(node.right));
+	}
+	
 	
 }
