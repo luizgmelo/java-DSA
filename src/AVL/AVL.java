05c83d34226a17e9f5218e7b9c7032cb5b6133cb
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-05-20 16:58:40 -0300

    feat: add addition Node operation AVL

--- src/AVL/AVL.java
+++ src/AVL/AVL.java
@@ -1,5 +1,51 @@
 package AVL;
 
 public class AVL {
+	private Node root;
+	private int size;
 	
+	public boolean isEmpty() {
+		return this.root == null;
+	}
+	
+	public void add(int element) {
+		this.size++;
+		
+		if (isEmpty()) {
+			this.root = new Node(element);
+			return;
+		} 
+			
+		Node aux = this.root;
+		
+		while (aux != null) {
+			if (aux.value > element) {
+				if (aux.left == null) {
+					Node newNode = new Node(element);
+					aux.left = newNode;
+					aux.left.parent = aux;
+					
+					Node unbalanced = checkUnbalanced(newNode);
+					if (unbalanced != null) callBestRotation(unbalanced);
+					
+					return;
+				}
+				aux = aux.left;
+			} else {
+				if (aux.right == null) {
+					Node newNode = new Node(element);
+					aux.right = newNode;
+					aux.right.parent = aux;
+					
+					Node unbalanced = checkUnbalanced(newNode);
+					if (unbalanced != null) callBestRotation(unbalanced);
+					
+					return;
+				}
+				aux = aux.right;
+			}
+		}
+		
+		
+	}
 }
