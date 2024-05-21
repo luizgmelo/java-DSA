6bc1539277113f26d7225529ca920ed0abe04581
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-05-21 09:47:57 -0300

    feat: add rotates and bfs for avl

--- src/AVL/AVL.java
+++ src/AVL/AVL.java
@@ -1,5 +1,9 @@
 package AVL;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class AVL {
 	private Node root;
 	private int size;
@@ -44,8 +48,104 @@ public class AVL {
 				}
 				aux = aux.right;
 			}
+		}	
+	}
+	
+	
+	public Node checkUnbalanced(Node node) {
+		Node aux = node;
+		
+		while (aux != null) {
+			if (!aux.isBalanced()) {
+				return aux;
+			} else if (aux.parent != null) {
+				aux = aux.parent;
+			} else {
+				break;
+			}
 		}
 		
+		return null;
+	}
+	
+	public void callBestRotation(Node unbalanced) {
+		Node x = unbalanced;
 		
+		if (x.isLeftPending()) {
+			Node y = x.left;
+			if (y.left != null) rotateRight(x);
+			else {
+				rotateLeft(y); rotateRight(x);
+			}
+		}
+		
+		if (x.isRightPending()) {
+			Node y = x.right;
+			if (y.right != null) rotateLeft(x);
+			else {
+				rotateRight(y); rotateLeft(x);
+			}
+		}
+	}
+	
+	public void rotateRight(Node node) {
+		Node newRoot = node.left;
+		newRoot.parent = node.parent;
+		
+		node.left = newRoot.right;
+		newRoot.right = node;
+		
+		node.parent = newRoot;
+		
+		if (newRoot.parent != null) {
+			if (newRoot.parent.left == node) {
+				newRoot.parent.left = newRoot;
+			} else {
+				newRoot.parent.right = newRoot;
+			}
+		} else {
+			this.root = newRoot;
+		}
+	}
+	
+	public void rotateLeft(Node node) {
+		Node newRoot = node.right;
+		newRoot.parent = node.parent;
+		
+		node.right = newRoot.left;
+		newRoot.left = node;
+		
+		node.parent = newRoot;
+		
+		if (newRoot.parent != null) {
+			if (newRoot.parent.left == node) {
+				newRoot.parent.left = newRoot;
+			} else {
+				newRoot.parent.right = newRoot;
+			}
+		} else {
+			this.root = newRoot;
+		}
+	}
+	
+	public ArrayList<Integer> bfs() {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		Deque<Node> queue = new LinkedList<Node>();
+		
+		if (!isEmpty()) {
+			queue.addLast(this.root);
+			while (!queue.isEmpty()) {
+				Node current = queue.removeFirst();
+				
+				list.add(current.value);
+				
+				if (current.left != null)
+					queue.addLast(current.left);
+				if (current.right != null)
+					queue.addLast(current.right);
+			}
+		}
+		
+		return list;
 	}
 }
