6df57eef5f8c9aa9cef3abeeb3484655a6e55fd0
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-05-28 12:07:15 -0300

    feat: add insertion sort algorithm

---
+++ algorithms/insertionSort/InsertionSort.java
@@ -0,0 +1,17 @@
+package insertionSort;
+
+public class InsertionSort {
+	public static void insertionSort(int[] v) {
+		for (int i = 1; i < v.length; i++) {
+			
+			int j = i;
+			
+			while (j > 0 && v[j] < v[j - 1]) {
+				int aux = v[j];
+				v[j] = v[j - 1];
+				v[j - 1] = aux;
+				j--;
+			}
+		}
+	}
+}
