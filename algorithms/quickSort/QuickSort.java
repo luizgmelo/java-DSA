c67af688e6ed9851b690b1469be948d0254701c6
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-05-29 16:38:00 -0300

    feat: add another way of partition called Partition Hoare

--- algorithms/quickSort/QuickSort.java
+++ algorithms/quickSort/QuickSort.java
@@ -3,13 +3,13 @@ package quickSort;
 public class QuickSort {
 	public static void quickSort(int[] values, int left, int right) {
 		if (left < right) {
-			int pivot = partition(values, left, right);
+			int pivot = partitionHoare(values, left, right);
 			quickSort(values, left, pivot - 1);
 			quickSort(values, pivot + 1, right);
 		}
 	}
-	
-	public static int partition(int[] values, int left, int right) {
+	// Lomuto partition
+	public static int partitionLomuto(int[] values, int left, int right) {
 		int range = right - left + 1;
 		int rand_pivot_index = (int)(Math.random() * range) + left;
 		
@@ -28,6 +28,31 @@ public class QuickSort {
 		swap(values, i, left);
 		
 		return i;
+	}
+	
+	// Hoare partition
+	public static int partitionHoare(int[] values, int left, int right) {
+		int pivot = values[left];
+		int i = left + 1;
+		int j = right;
+		
+		while (i <= j) {
+			while (i <= j && values[i] <= pivot) {
+				i++;
+			}
+			
+			while (i <= j && values[j] > pivot) {
+				j--;
+			}
+			
+			if (i < j) {
+				swap(values, i, j);
+			}
+		}
+		
+		swap(values, left, j);
+		
+		return j;
 	}
 	
 	public static void swap(int[] values, int a, int b) {
