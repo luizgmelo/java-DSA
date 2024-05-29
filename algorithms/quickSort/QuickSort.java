a18f283fa1faf7a64ba79b884ebcb67ee4c94441
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-05-29 15:48:30 -0300

    feat: add quickSort method quickSort algorithm

--- algorithms/quickSort/QuickSort.java
+++ algorithms/quickSort/QuickSort.java
@@ -1,7 +1,15 @@
 package quickSort;
 
 public class QuickSort {
-	public static void partition(int[] values, int left, int right) {
+	public static void quickSort(int[] values, int left, int right) {
+		if (left < right) {
+			int pivot = partition(values, left, right);
+			quickSort(values, left, pivot - 1);
+			quickSort(values, pivot + 1, right);
+		}
+	}
+	
+	public static int partition(int[] values, int left, int right) {
 		int pivot = values[left];
 		int i = left;
 		
@@ -13,6 +21,8 @@ public class QuickSort {
 		}
 		
 		swap(values, i, left);
+		
+		return i;
 	}
 	
 	public static void swap(int[] values, int a, int b) {
